98c125fc6a7fd59bae09b96492ccc7f4e517048d
LuisBor-MS <luisbor@microsoft.com>
2025-09-08 16:30:18 -0600

    Committing 3 items from workspace 7068e8ef-4071-4b46-9ed6-bd13210ed606

--- LuisBor/GithubNotebookSyncDemo.Notebook/notebook-content.py
+++ LuisBor/GithubNotebookSyncDemo.Notebook/notebook-content.py
@@ -1,2 +1,19 @@
 # Fabric notebook source
 
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   }
+# META }
+
+# CELL ********************
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
