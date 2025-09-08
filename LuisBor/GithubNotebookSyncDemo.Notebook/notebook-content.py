57ae68daa2f24580b0a651f1d134df86ca5c580e
LuisBor-MS <luisbor@microsoft.com>
2025-09-08 16:19:29 -0600

    commit

--- LuisBor/GithubNotebookSyncDemo.Notebook/notebook-content.py
+++ LuisBor/GithubNotebookSyncDemo.Notebook/notebook-content.py
@@ -1,26 +1,32 @@
-# # AdventureWorks Sales Analysis
+# %%
+# AdventureWorks Sales Analysis
 # This notebook demonstrates data loading, transformation, and visualization using AdventureWorks data from the Lakehouse `AIAdvWks_Lakehouse`.
 
+# %%
 # Import necessary libraries
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set(style='whitegrid')
 
+# %%
 # Load data from Lakehouse
 dimcustomer = spark.read.table("AIAdvWks_Lakehouse.dimcustomer")
 factinternetsales = spark.read.table("AIAdvWks_Lakehouse.factinternetsales")
 dimgeography = spark.read.table("AIAdvWks_Lakehouse.dimgeography")
 
+# %%
 # Convert to pandas DataFrames
 dimcustomer_pd = dimcustomer.toPandas()
 factinternetsales_pd = factinternetsales.toPandas()
 dimgeography_pd = dimgeography.toPandas()
 
+# %%
 # Merge datasets on CustomerKey
 sales_data = pd.merge(factinternetsales_pd, dimcustomer_pd, on='CustomerKey', how='inner')
 sales_data = pd.merge(sales_data, dimgeography_pd[['GeographyKey', 'City']], on='GeographyKey', how='left')
 
+# %%
 # Sales by City
 city_sales = sales_data.groupby('City')['SalesAmount'].sum().reset_index()
 city_sales = city_sales.sort_values(by='SalesAmount', ascending=False)
@@ -33,6 +39,7 @@ plt.xticks(rotation=45, ha='right')
 plt.tight_layout()
 plt.show()
 
+# %%
 # Sales by Gender
 gender_sales = sales_data.groupby('Gender')['SalesAmount'].sum().reset_index()
 plt.figure(figsize=(6, 4))
@@ -43,6 +50,7 @@ plt.ylabel('Sales Amount')
 plt.tight_layout()
 plt.show()
 
+# %%
 # Yearly Sales Trend
 sales_data['OrderYear'] = pd.to_datetime(sales_data['OrderDate']).dt.year
 yearly_sales = sales_data.groupby('OrderYear')['SalesAmount'].sum().reset_index()
